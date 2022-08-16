5c96154baeb176a5ac3da3114f0f6d37f394d078
johnkhalife <jpa.khalife@gmail.com>
2022-08-16 09:52:24 -0230

    - created an undo function by pressing u - made the buttons round (omg that's crazy) - Made buttons size based on the screen size - made buttons look better - added new difficulty settings that controls the number of undos - buttons react differently when selected - Added titles for the options menu and start screen - added more images -added menu floaters - added background to game - created victory screen - Added pybuild - created tutorial screen

---
+++ src/main/scripts/move.py
@@ -0,0 +1,10 @@
+# This is an object that stores a colour and a value associated with the colour. This will be used to store colours in the stack owned by the test tube
+
+#creating the colour class
+class Move:
+
+    #initialization function
+    def __init__(self, initial,final, units):
+        self.initial = initial
+        self.final = final
+        self.units = units
