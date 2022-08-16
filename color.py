6bead4433c5cbdae7843b744a1cc283d907179a4
johnkhalife <jpa.khalife@gmail.com>
2022-08-16 09:56:10 -0230

    - another pybuild update

--- color.py
+++
@@ -1,8 +0,0 @@
-# This is an object that stores a colour and a value associated with the colour. This will be used to store colours in the stack owned by the test tube
-
-#creating the colour class
-class Colour:
-
-    #initialization function
-    def __init__(self, colour):
-        self.colour = colour
